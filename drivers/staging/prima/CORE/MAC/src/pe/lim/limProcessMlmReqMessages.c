47a98fe60d37ed4d9153c3e6332e57859bf4764b
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 17:19:01 -0700

    prima: Fix misleading indentation warning in limProcessMlmRemoveKeyReq
    Change-Id: I63ec9745b9e460118c5decc17ae507d209d0d513

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
@@ -4006,9 +4006,9 @@ tLimMlmRemoveKeyCnf  mlmRemoveKeyCnf;
 
       goto end;
   }
-  else
+  else {
     staIdx = pStaDs->staIndex;
-  
+  }
 
 
     psessionEntry->limMlmState = eLIM_MLM_WT_REMOVE_STA_KEY_STATE;
